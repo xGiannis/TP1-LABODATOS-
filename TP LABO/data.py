a75c4785558988837cbd01813d457b43a26e5109
Sebastian <soutosebastianma@gmail.com>
2024-10-11 15:12:07 -0300

    I ii)

--- TP LABO/data.py
+++ TP LABO/data.py
@@ -16,10 +16,10 @@ import seaborn as sns           # Para graficar histograma
 
 #C:\Users\usuario\Desktop\TP1-LABODATOS-\TP LABO\lista-sedes-basicos.csv
 #gian:
-miprefijo="C:/Users/usuario/Desktop/TP1-LABODATOS-/TP LABO/" 
+#miprefijo="C:/Users/usuario/Desktop/TP1-LABODATOS-/TP LABO/" 
 
 #seba:
-#miprefijo = "C:\\Users\\Sebastián\\Documents\\LaboDeDatos\\TP1\\" 
+miprefijo = "C:\\Users\\Sebastián\\Documents\\LaboDeDatos\\TP1\\" 
 
 archivo_completo=  "lista-sedes-completos.csv"
 
@@ -37,23 +37,31 @@ datos_completos= pd.read_csv(miprefijo+archivo_completo, on_bad_lines='skip')
 
 datos_migraciones = pd.read_csv(miprefijo+archivo_migraciones)
 
-#%%
 
+
+#%%
 #Armado de las tablas del Modelo Relacional:
 
-# ######################PAIS##################:
+############PAIS#############:
 
 #Primero saco todos los valores que no se pueden convertir a decimal de la columna 2000
-
+consulta_sql2 = """
+                 SELECT * 
+                 FROM datos_migraciones
+                 WHERE TRY_CAST("2000 [2000]" AS DECIMAL) IS NOT NULL;
+                """
+                
 consulta_sql20 = """
                   SELECT "Country Origin Name", "Country Origin Code",
                          "Migration by Gender Name", "Migration by Gender Code",
-                         "Country Dest Name", "Country Dest Code", "1960 [1960]", "1970 [1970]",
-                         "1980 [1980]", "1990 [1990]", REPLACE ("2000 [2000]", '..', '0') AS "2000 [2000]"
+                         "Country Dest Name", "Country Dest Code", REPLACE ("1960 [1960]", '..', '0') AS "1960 [1960]" , REPLACE ("1970 [1970]", '..', '0') AS "1970 [1970]",
+                         REPLACE("1980 [1980]",'..','0') AS "1980 [1980]", REPLACE("1990 [1990]",'..','0') AS "1990 [1990]", REPLACE ("2000 [2000]", '..', '0') AS "2000 [2000]"
                   FROM datos_migraciones;
                  """
 
+
 datos_migraciones2 = sql^consulta_sql20
+
 
 #Tomo las inmigraciones y las emigraciones y sumo.
     
@@ -79,7 +87,8 @@ consulta_sql = """
                 SELECT DISTINCT i.codigo, (i.inmigraciones00 - e.emigraciones00) AS flujo_mundo
                 FROM inmigraciones00 AS i
                 INNER JOIN emigraciones00 AS e
-                ON i.codigo = e.codigo;
+                ON i.codigo = e.codigo
+                ORDER BY i.codigo;
                """
 
 migraciones00 = sql^consulta_sql
@@ -113,6 +122,60 @@ consulta_sql = """
 
 migraciones00ARG = sql^consulta_sql
 
+#ahora hago una tabla que tenga flujo migratorio de 1960 hasta 2000 (para el punto I ii)
+
+#calculo las emigraciones
+consulta_sql = """
+                SELECT DISTINCT "Country Origin Code" AS codigo, SUM(CAST("1960 [1960]" AS DECIMAL)) AS emigraciones60,
+                                SUM(CAST("1970 [1970]" AS DECIMAL)) AS emigraciones70,
+                                SUM(CAST("1980 [1980]" AS DECIMAL)) AS emigraciones80,
+                                SUM(CAST("1990 [1990]" AS DECIMAL)) AS emigraciones90,
+                                SUM(CAST("2000 [2000]" AS DECIMAL)) AS emigraciones00
+                FROM datos_migraciones2
+                GROUP BY "Country Origin Code";
+               """
+               
+emigraciones = sql^consulta_sql
+
+#calculo la inmigraciones
+consulta_sql = """
+                SELECT DISTINCT "Country Dest Code" AS codigo, SUM(CAST("1960 [1960]" AS DECIMAL)) AS inmigraciones60,
+                                SUM(CAST("1970 [1970]" AS DECIMAL)) AS inmigraciones70,
+                                SUM(CAST("1980 [1980]" AS DECIMAL)) AS inmigraciones80,
+                                SUM(CAST("1990 [1990]" AS DECIMAL)) AS inmigraciones90,
+                                SUM(CAST("2000 [2000]" AS DECIMAL)) AS inmigraciones00
+                FROM datos_migraciones2
+                GROUP BY "Country Dest Code";
+               """
+               
+inmigraciones = sql^consulta_sql
+
+
+#calculo el flujo migratorio
+consulta_sql = """
+                SELECT DISTINCT i.codigo, 
+                                (i.inmigraciones60 - e.emigraciones60) AS flujo_mundo60,
+                                (i.inmigraciones70 - e.emigraciones70) AS flujo_mundo70,
+                                (i.inmigraciones80 - e.emigraciones80) AS flujo_mundo80,
+                                (i.inmigraciones90 - e.emigraciones90) AS flujo_mundo90,
+                                (i.inmigraciones00 - e.emigraciones00) AS flujo_mundo00
+                                
+                FROM inmigraciones AS i
+                INNER JOIN emigraciones AS e
+                ON i.codigo = e.codigo
+                ORDER BY i.codigo;
+               """
+
+migraciones = sql^consulta_sql
+
+#hago la suma de los flujos migratorios por cada decada
+consulta_sql = """
+                SELECT DISTINCT codigo, (flujo_mundo60 + flujo_mundo70 + flujo_mundo80 + flujo_mundo90 +flujo_mundo00) flujo60_00
+                FROM migraciones
+                ORDER BY codigo ASC;
+               """
+               
+flujo_total = sql^consulta_sql 
 #Ahora voy a armar una tabla que tenga Codigo, Pais y region geografica
 
 
@@ -127,12 +190,15 @@ info_pais = sql^consulta_sql
 #Finalmente armo la relacion PAIS.
 
 consulta_sql = """
-                SELECT DISTINCT i.nombre_pais, i.region_geografica, m1.flujo_mundo, m2.flujo_ARG
+                SELECT DISTINCT i.nombre_pais, i.region_geografica, m1.flujo_mundo, m2.flujo_ARG, f.flujo60_00
                 FROM info_pais as i
                 INNER JOIN migraciones00 AS m1
                 ON i.pais_iso_3 = m1.codigo
                 INNER JOIN migraciones00ARG AS m2
-                ON i.pais_iso_3 = m2.codigo;
+                ON i.pais_iso_3 = m2.codigo
+                INNER JOIN flujo_total AS f
+                ON i.pais_iso_3 = f.codigo
+                ORDER BY i.nombre_pais;
                """
 
 Pais = sql^consulta_sql
@@ -455,6 +521,24 @@ plt.grid(True,linestyle="--",linewidth=0.5)
 #ax.set_yticks([])                          # Remueve los ticks del eje y
 #ax.bar_label(ax.containers[0], fontsize=8)   # Agrega la etiqueta a cada barra
 
+#%%
+
+#ii)
+
+medianas = Pais.groupby('region_geografica')['flujo_mundo'].median().sort_values(ascending=False)
+
+ax = sns.boxplot(x="region_geografica", 
+                 y="flujo60_00",  
+                 data=Pais,
+                 order = medianas.index,
+                 palette = "colorblind")
+
+ax.set_title('Flujo Migratorio Por Región')
+ax.set_xlabel('Región Geográfica')
+ax.set_ylabel('Flujo Migratorio')
+ 
+plt.xticks(rotation=90)
+plt.grid(True,linestyle="--",linewidth=0.5)
 
 
 
